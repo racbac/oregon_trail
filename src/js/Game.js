2f0b0ca1cdbb38367bf495eeec438ed57cca2c8b
ChenKuo <ckuo1@umbc.edu>
2017-04-13 12:56:39 -0400

    store

--- src/js/Game.js
+++ src/js/Game.js
@@ -160,13 +160,56 @@ var Game={
       Game.waitForInput(null,Game.scenes.chooseDepartureMonth);
     },
     MattStore:function(){
-      document.getElementById("game").innerHTML ="Matt store not done yet"
+      document.getElementById("game").innerHTML ="This is Matt's store. It will show a few pages with information about the products. Press Enter to read through them.";
+      Game.waitForInput(null,function(){
+        document.getElementById("game").innerHTML ="store information page 1. press enter for next."
+        Game.waitForInput(null,function(){
+          document.getElementById("game").innerHTML ="store information page2. press enter to shop."
+          Game.waitForInput(null,function(){
+            document.getElementById("game").innerHTML =
+            `<div id="mattstore">
+              <div>
+                Matt's General Store<br>
+                Independence, Missouri<br>
+                date/data/date
+              </div>
+              <ol style="width: 500px">
+                <li>Oxen<span id="oxen_bill" style="float: right">$0.00</span></li>
+                <li>Food<span id="food_bill" style="float: right">$0.00</span></li>
+                <li>Clothing<span id="clothing_bill" style="float: right">$0.00</span></li>
+                <li>Ammo<span id="ammo_bill" style="float: right">$0.00</span></li>
+                <li>Spare Parts<span id="spare_bill" style="float: right">$0.00</span></li>
+              </ol>
+              <div>Total Bill: <span id="total_bill" style="float: right">$0.00</span></div>
+              <br>
+              <div>Amount you have:<span id="money" style="float: right">$0.00</span></div>
+              <p>Which item would you like to buy?<span id="input"></span></p>
+              <p>Press SPACE to leave store</p>
+            </div>`;
+            Game.waitForInput(document.getElementById("input"),function(choice){
+
+            });
+          });
+        });
+      });
     },
     BuySupply:function(){
 
     },
     Journey:function(){
-
+      document.getElementById("game").innerHTML =
+        `<div id="journey">
+          <div>animation goes here</div>
+          <div>press ENTER to size up the situation</div>
+          <div>
+            Date:<br>
+            Weather:<br>
+            Health:<br>
+            Food:<br>
+            Next Landmark:<br>
+            Miles Traveled:<br>
+          </div>
+        </div>`;
     },
     Fishing: function(){
 
