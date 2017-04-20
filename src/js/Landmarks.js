ef7361e327471ac0a2af1a547c00199fb111cee3
bdubin1 <bdubin1@umbc.edu>
2017-04-20 13:34:13 -0400

    m

--- src/js/Landmarks.js
+++ src/js/Landmarks.js
@@ -1,3 +1,23 @@
+var landmarkNames=["Independence",
+"the Kansas River Crossing",
+"the Big Blue River Crossing",
+"Fort Kearney",
+"Chimney Rock",
+"Fort Laramie",
+"Independence Rock",
+"South Pass",
+"Fort Bridger",
+"the Green River Crossing",
+"Soda Springs",
+"Fort Hall",
+"the Snake River Crossing",
+"Fort Boise",
+"Grande Ronde in the Blue Mountains",
+"Blue Mountains",
+"Fort Walla Walla",
+"The Dalles",
+"the Willamette Valley",
+"Oregon City"]
 function Landmarks(){
 	this.name="Independence";
 	this.distance=0;
