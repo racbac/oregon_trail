05e08c7a4e9a8660ff41909721352c4ca4a70e8b
bdubin1 <bdubin1@umbc.edu>
2017-04-26 13:08:53 -0400

    k

--- src/js/RiverCrossingGame.js
+++ src/js/RiverCrossingGame.js
@@ -1,14 +1,19 @@
 var myGamePiece;
+var myObstacles=[];
 function startGame() {
     
     //myGamePiece=new component(30,30,"wagonOnRiver.gif",10,120,"image");
     
     myGameArea.start();
-    myGamePiece=new component(70,30,"../img/wagonOnRiver.gif",10,120,"image");
+    myGamePiece=new component(90,30,"../img/wagonOnRiver.gif",20,70,"image");
+    //myObstacle  = new component(20, 10, "../img/rock.gif", 200, 100,"image");   
     myGamePiece.update();
     //myGamePiece = new component(30, 30, "red", 10, 120);
 }
-
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
+    return false;
+}
 var myGameArea = {
     canvas : document.getElementById("canvas"),
     start : function() {
@@ -18,9 +23,15 @@ var myGameArea = {
         document.body.insertBefore(canvas, document.body.childNodes[0]);
     	  this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
         },
     clear : function() {
-        context.clearRect(myGamePiece.x, myGamePiece.y, myGamePiece.width, myGamePiece.height);
+        context.clearRect(0, 0, canvas.width, canvas.height);
     },
     stop : function() {
         clearInterval(this.interval);
@@ -56,10 +67,57 @@ function component(width, height, color, x, y, type) {
         this.x += this.speedX;
         this.y += this.speedY;        
     }
+  this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) ||
+               (mytop > otherbottom) ||
+               (myright < otherleft) ||
+               (myleft > otherright)) {
+           crash = false;
+        }
+        return crash;
+    }
 }
 function updateGameArea() {
+
+    //myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0; 
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    var x, y,minHeight, maxHeight;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (myGamePiece.crashWith(myObstacles[i])) {
+            myGameArea.stop();
+            return;
+        }
+    }
     myGameArea.clear();
-    myGamePiece.x += 1;
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)||everyinterval(100)) {
+        x = canvas.width;
+        minHeight = 30;
+        maxHeight = 160;
+        //y = canvas.height - 100
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        myObstacles.push(new component(20, 10, "../img/rock.gif", x, height,"image"));
+    }
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].x += -1;
+        myObstacles[i].update();
+    }
+
     myGamePiece.newPos();
     myGamePiece.update();
+
 }
