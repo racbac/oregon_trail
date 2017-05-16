78acd09d5fefcf725b56917c7e88fcea3ba98a5f
Rachel Backert <bac2@umbc.edu>
2017-05-16 16:31:46 -0400

    delete players that get bumped off the top ten

--- src/php/updateTopTen.php
+++ src/php/updateTopTen.php
@@ -7,17 +7,19 @@
     else if ($score <= 6000) {$rating = "Adventurer"; }
     else { $rating = "Trail guide"; }
 
-    $sql = "SELECT MIN(`points`) as points from `oregon_top_ten`";
-    $rs = mysql_fetch_array($COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']));
-    if ($rs[0] < $score) {
-        $sql = "INSERT INTO `oregon_top_ten`(`name`, `points`, `rating`) VALUES ('".$_GET['name']."','".$score."','".$rating."')";
+    $sql = "SELECT `key`, MIN(`points`) as points from `oregon_top_ten`";
+    $rs = mysql_fetch_assoc($COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']));
+    if ($rs['points'] < $score) {
+        $sql = "DELETE FROM `oregon_top_ten` WHERE `key` = '".$rs['key']."'".
+        $rs = $COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
+        $sql = "INSERT INTO `oregon_top_ten` (`name`, `points`, `rating`) VALUES ('".$_GET['name']."','".$score."','".$rating."')";
         $rs = $COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
         echo("true");
     } else {
         $sql = "SELECT COUNT(*) `oregon_top_ten`";
-        $rs = mysql_fetch_array($COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']));
+        $rs = mysql_fetch_row($COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']));
         if ($rs[0] < 10) {
-            $sql = "INSERT INTO `oregon_top_ten`(`name`, `points`, `rating`) VALUES ('".$_GET['name']."','".$score."','".$rating."')";
+            $sql = "INSERT INTO `oregon_top_ten`(`name`, `points`, `rating`) VALUES ('".$_GET['name']."', '".$score."', '".$rating."')";
             $rs = $COMMON->executeQuery($sql, $_SERVER['SCRIPT_NAME']);
             echo("true");
         } else {
